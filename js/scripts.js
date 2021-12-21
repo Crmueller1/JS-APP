fb319aaec89729a379b352a5b8bed89368e44218
Crmueller1 <crmueller20@gmail.com>
2021-12-21 12:54:40 -0600

    Updated JS to show on screen/web
    1. Capitalized names
    2. Changed height # format
    3. added an Initialization, Condition, and a Action to show name, height, and type from the array.
    4. added <br> to bottom of js to make it read easier

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-let pokemonList = [{ name: "charmander", height: "2'00", type: "fire" }, { name: "squirtle", height: "1'08", type: "water" }, { name: "bulbasaur", height: "2'04", type: ["grass", "poison"] }];
+let pokemonList = [
+    { name: "Charmander", height: "2", type: "fire" },
+    { name: "Squirtle", height: "1.08", type: "water" },
+    { name: "Bulbasaur", height: "2.04", type: ["grass", "poison"] }
+];
+
+
+
+
+// Initialization= let i=0,  
+// Condition= i < pokemonList.length;
+// Action = i++ which is  i=i+1
+
+for (let i = 0; i < pokemonList.length; i++) {
+
+    document.write(pokemonList[i].name + " " + "(Height: " +
+        pokemonList[i].height + ", Type: " + pokemonList[i].type + ") ");
+
+    // if the pokemon is greater than or equal to, write "This pokemon is huge!!"
+    if (pokemonList[i].height > 1.5) {
+
+        document.write('This pokemon is huge!!');
+    }
+    // line breaks twice after each document.write to make it easier to read
+    document.write('<br><br>');
+}
